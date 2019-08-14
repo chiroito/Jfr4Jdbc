a657fe29fdb1a4327d4245e4f440bfa9e786dec8
Chihiro Ito <chiroito107@gmail.com>
2019-08-14 18:36:01 +0900

    Forgot commit.

---
+++ src/main/java/chiroito/jfr4jdbc/Jfr4JdbcRuntimeException.java
@@ -0,0 +1,8 @@
+package chiroito.jfr4jdbc;
+
+public class Jfr4JdbcRuntimeException extends RuntimeException {
+
+    public Jfr4JdbcRuntimeException(String message) {
+        super(message);
+    }
+}
